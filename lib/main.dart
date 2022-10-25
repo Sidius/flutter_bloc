82e5489cc4c86e43bb46f893b0f0d643d6cd962a
Romanets Pavel <pavelroma24@gmail.com>
2022-10-25 14:13:35 +0300

    add: lesson 3 (read, watch, select)

--- lib/main.dart
+++ lib/main.dart
@@ -18,95 +18,120 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: Wrapper(),
     );
   }
 }
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CounterBloc>(
+          create: (context) => CounterBloc(),
+        ),
+        BlocProvider<UserBloc>(
+          create: (context) => UserBloc(),
+        ),
+      ],
+      child: MyHomePage(),
+    );
+  }
+}
+
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final counterBloc = CounterBloc();
-    final userBloc = UserBloc();
+    final counterBloc = context.read<CounterBloc>();
+    
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+              onPressed: () {
+                // final counterBloc = BlocProvider.of<CounterBloc>(context);
+                counterBloc.add(CounterIncEvent());
+              },
+              icon: Icon(Icons.plus_one)
+          ),
+          IconButton(
+              onPressed: () {
+                final counterBloc = context.read<CounterBloc>();
+                counterBloc.add(CounterDecEvent());
+              },
+              icon: Icon(Icons.exposure_minus_1)
+          ),
+          IconButton(
+              onPressed: () {
+                final userBloc = context.read<UserBloc>();
+                final counterBloc = context.read<CounterBloc>();
+                userBloc.add(UserGetUsersEvent(counterBloc.state));
+              },
+              icon: Icon(Icons.person)
+          ),
+          IconButton(
+              onPressed: () {
+                final userBloc = context.read<UserBloc>();
+                final counterBloc = context.read<CounterBloc>();
+                userBloc.add(UserGetUsersJobEvent(counterBloc.state));
+              },
+              icon: Icon(Icons.work)
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              BlocBuilder<CounterBloc, int>(
+                // bloc: counterBloc,
+                builder: (context, state) {
+                  // final bloc = context.watch<UserBloc>();
+                  final users = context.select((UserBloc bloc) => bloc.state.users);
 
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<CounterBloc>(
-          create: (context) => counterBloc,
-        ),
-        BlocProvider<UserBloc>(
-          create: (context) => userBloc,
-        ),
-      ],
-      child: Scaffold(
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-                onPressed: () {
-                  counterBloc.add(CounterIncEvent());
-                },
-                icon: Icon(Icons.plus_one)
-            ),
-            IconButton(
-                onPressed: () {
-                  counterBloc.add(CounterDecEvent());
-                },
-                icon: Icon(Icons.exposure_minus_1)
-            ),
-            IconButton(
-                onPressed: () {
-                  userBloc.add(UserGetUsersEvent(counterBloc.state));
-                },
-                icon: Icon(Icons.person)
-            ),
-            IconButton(
-                onPressed: () {
-                  userBloc.add(UserGetUsersJobEvent(counterBloc.state));
-                },
-                icon: Icon(Icons.work)
-            )
-          ],
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                BlocBuilder<CounterBloc, int>(
-                  bloc: counterBloc,
-                  builder: (context, state) {
-                    return Text(state.toString(),
-                      style: TextStyle(fontSize: 33),
-                    );
-                  },
-                ),
-                BlocBuilder<UserBloc, UserState>(
-                  bloc: userBloc,
-                  builder: (context, state) {
-                    final users = state.users;
-                    final jobs = state.jobs;
-
-                    return Column(
-                      children: [
-                        if (state.isLoading) CircularProgressIndicator(),
-                        if (users.isNotEmpty)
-                          ...users.map((e) => Text(e.name,
+                  return Column(
+                    children: [
+                      Text(state.toString(),
+                        style: TextStyle(fontSize: 33),
+                      ),
+                      if (users.isNotEmpty)
+                        ...users.map((e) =>
+                            Text(e.name,
                               style: TextStyle(fontSize: 33),
                             ),
-                          ),
-                        if (jobs.isNotEmpty)
-                          ...jobs.map((e) => Text(e.name,
+                        ),
+                    ],
+                  );
+                },
+              ),
+              BlocBuilder<UserBloc, UserState>(
+                // bloc: userBloc,
+                builder: (context, state) {
+                  final users = state.users;
+                  final jobs = state.jobs;
+
+                  return Column(
+                    children: [
+                      if (state.isLoading) CircularProgressIndicator(),
+
+                      if (jobs.isNotEmpty)
+                        ...jobs.map((e) =>
+                            Text(e.name,
                               style: TextStyle(fontSize: 33),
                             ),
-                          ),
-                      ],
-                    );
-                  },
-                ),
-              ],
-            ),
+                        ),
+                    ],
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
